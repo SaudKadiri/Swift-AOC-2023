d24a2ef9ce90b0febfe04f0980ba2b8cd5cd8230
Saud <34000860+SaudKadiri@users.noreply.github.com>
2023-12-02 13:19:38 +0530

    Day00 using two pointer

--- Sources/Day00.swift
+++ Sources/Day00.swift
@@ -5,21 +5,78 @@ struct Day00: AdventDay {
   var data: String
 
   // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
-    }
+  var lines: [String] {
+    data.split(separator: "\n").map { String($0) }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    lines.reduce(into: 0) { sum, line in
+      var (lo, hi) = (line.startIndex, line.index(before: line.endIndex))
+      var firstDigit: Int? = nil
+      var lastDigit: Int? = nil
+  
+      while lo <= hi, firstDigit == nil || lastDigit == nil {
+        if firstDigit == nil {
+          if let d = line[lo].wholeNumberValue {
+            firstDigit = d
+          } else {
+            lo = line.index(after: lo)
+          }
+        }
+        if lastDigit == nil {
+          if let d = line[hi].wholeNumberValue {
+            lastDigit = d
+          } else {
+            hi = line.index(before: hi)
+          }
+        }
+      }
+      let calibrationValue = firstDigit! * 10 + lastDigit!
+      sum += calibrationValue
+    }
   }
 
-  // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    lines.reduce(into: 0) { sum, line in
+      var (lo, hi) = (line.startIndex, line.index(before: line.endIndex))
+      var firstDigit: Int? = nil
+      var lastDigit: Int? = nil
+      let spellings: [Int: String] = [
+        1: "one", 2: "two", 3: "three", 
+        4: "four", 5: "five", 6: "six", 
+        7: "seven", 8: "eight", 9: "nine"
+      ]
+      var lstring = ""
+      var rstring = ""
+      while lo <= hi, firstDigit == nil || lastDigit == nil {
+        for (d, spelling) in spellings {
+          if lstring.hasSuffix(spelling) {
+            firstDigit = d
+          }
+          if rstring.hasPrefix(spelling) {
+            lastDigit = d
+          }
+        }
+        if firstDigit == nil {
+          if let d = line[lo].wholeNumberValue {
+            firstDigit = d
+          } else {
+            lstring = "\(lstring)\(line[lo])"
+            lo = line.index(after: lo)
+          }
+        }
+        if lastDigit == nil {
+          if let d = line[hi].wholeNumberValue {
+            lastDigit = d
+          } else {
+            rstring = "\(line[hi])\(rstring)"
+            hi = line.index(before: hi)
+          }
+        }
+      }
+      let calibrationValue = firstDigit! * 10 + lastDigit!
+      sum += calibrationValue
+    }
   }
 }
